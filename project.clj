ae37c81469675d376add316691262240bfc54ce7
inaimathi <leo.zovic@gmail.com>
2017-10-05 22:58:30 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject acknowledge "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+
+                 [trivial-warning "1.0.3"]
+                 [bidi "2.1.2"]])
---
+++ src/acknowledge/core.clj
@@ -0,0 +1,4 @@
+(ns acknowledge.core
+  (:require [acknowledge.handlers :refer [mk-handler-table mk-routes-data
+                                          intern-static! intern-handler-fn! link-to
+                                          routes-handler]]))
